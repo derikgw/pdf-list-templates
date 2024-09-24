4bb53e00393f3289c9946cc78d4ce87b4ac5d4b6
Derik Wilson <derikwilson6@gmail.com>
2024-09-23 21:37:29 -0400

    Corrected the S3 path logic and corrected logging to lambda.

--- list_templates_function.py
+++ list_templates_function.py
@@ -4,13 +4,20 @@ import logging
 from template_list_service import list_pdf_templates  # Import the function from the new module
 
 # Set up logging
-logging.basicConfig(level=logging.INFO)
+if logging.getLogger().hasHandlers():
+    # The Lambda environment pre-configures a handler logging to stderr. If a handler is already configured,
+    # `.basicConfig` does not execute. Thus we set the level directly.
+    logging.getLogger().setLevel(logging.INFO)
+else:
+    logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger()
 
 def lambda_handler(event, context):
     try:
         # Define S3 bucket and prefix
-        bucket_name = 'aws-sam-cli-managed-default-samclisourcebucket-kdvjqzoec6pg'
+        bucket_name = 's3://aws-sam-cli-managed-default-samclisourcebucket-kdvjqzoec6pg'
+
         prefix = 'pdf_templates/'
 
         # Log the incoming event for debugging
--- template_list_service.py
+++ template_list_service.py
@@ -4,7 +4,13 @@ import logging
 import os
 
 # Set up logging
-logging.basicConfig(level=logging.INFO)
+if logging.getLogger().hasHandlers():
+    # The Lambda environment pre-configures a handler logging to stderr. If a handler is already configured,
+    # `.basicConfig` does not execute. Thus we set the level directly.
+    logging.getLogger().setLevel(logging.INFO)
+else:
+    logging.basicConfig(level=logging.INFO)
+
 logger = logging.getLogger()
 
 # Initialize the S3 client
